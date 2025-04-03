9f96fc32b777a28569c1e6e6f4ad02db98fc5649
IR25721 <zunik0421@gmail.com>
2025-04-03 10:52:44 +0900

    add variable

--- src/lib.rs
+++ src/lib.rs
@@ -2,52 +2,51 @@ use integrate::prelude::legendre_rule;
 
 #[warn(dead_code)]
 pub struct PositionAndVelocity {
-    x: fn(f32) -> f32,
-    y: fn(f32) -> f32,
-    dx: fn(f32) -> f32,
-    dy: fn(f32) -> f32,
+    x: fn(f32, f32) -> f32,
+    y: fn(f32, f32) -> f32,
+    dx: fn(f32, f32) -> f32,
+    dy: fn(f32, f32) -> f32,
 }
 
 impl PositionAndVelocity {
     pub fn new(
-        x: fn(f32) -> f32,
-        y: fn(f32) -> f32,
-        dx: fn(f32) -> f32,
-        dy: fn(f32) -> f32,
+        x: fn(f32, f32) -> f32,
+        y: fn(f32, f32) -> f32,
+        dx: fn(f32, f32) -> f32,
+        dy: fn(f32, f32) -> f32,
     ) -> Self {
         Self { x, y, dx, dy }
     }
-    
-    pub fn s(&self, t: f32) -> f32 {
-        let func = |theta: f32| ((self.dx)(theta).powi(2) + (self.dy)(theta).powi(2)).sqrt();
+
+    pub fn s(&self, t: f32, theta: f32) -> f32 {
+        let func = |t: f32| ((self.dx)(t, theta).powi(2) + (self.dy)(t, theta).powi(2)).sqrt();
         let lower_limit = 0.;
         let upper_limit = t;
         let n = 50_usize;
         legendre_rule(func, lower_limit, upper_limit, n) as f32
     }
-    
-    fn n(&self, t: f32) -> Vec<f32> {
-        let ds = ((self.dx)(t).powi(2) + (self.dy)(t).powi(2)).sqrt();
+
+    fn n(&self, t: f32, theta: f32) -> Vec<f32> {
+        let ds = ((self.dx)(t, theta).powi(2) + (self.dy)(t, theta).powi(2)).sqrt();
         if ds < 1e-6 {
             vec![1., 0.]
         } else {
-            vec![-(self.dy)(t) / ds, (self.dx)(t) / ds]
+            vec![-(self.dy)(t, theta) / ds, (self.dx)(t, theta) / ds]
         }
     }
 
-    fn normal_offset(&self, t: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let current_s = self.s(t);
-        a(t) * (current_s / ta).sin()
+    fn normal_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
+        let current_s = self.s(t, theta);
+        a(theta) * (current_s / ta).sin()
     }
 
-    pub fn x_offset(&self, t: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let normal = self.n(t);
-        (self.x)(t) + self.normal_offset(t, a, ta) * normal[0]
+    pub fn x_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
+        let normal = self.n(t, theta);
+        (self.x)(t, theta) + self.normal_offset(t, theta, a, ta) * normal[0]
     }
-    
-    pub fn y_offset(&self, t: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let normal = self.n(t);
-        (self.y)(t) + self.normal_offset(t, a, ta) * normal[1]
+
+    pub fn y_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
+        let normal = self.n(t, theta);
+        (self.y)(t, theta) + self.normal_offset(t, theta, a, ta) * normal[1]
     }
 }
-
