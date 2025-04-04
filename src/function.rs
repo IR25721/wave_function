76fdac129425f80ce451e6d10482e6579aa8a5dd
IR25721 <zunik0421@gmail.com>
2025-04-05 02:44:34 +0900

    various_update

--- src/function.rs
+++
@@ -1,6 +0,0 @@
-pub trait Trajectory<F>
-where
-    F: Fn(f32, f32) -> f32,
-{
-    pub fn get_trajectory(&self, values: Vec<f32>) -> Self;
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,44 @@
 use integrate::prelude::legendre_rule;
+use nannou::glam::Vec2;
 
-#[warn(dead_code)]
-pub struct PositionAndVelocity {
-    x: Box<dyn Fn(f32, f32) -> f32 + Send + Sync>,
-    y: Box<dyn Fn(f32, f32) -> f32 + Send + Sync>,
-    dx: Box<dyn Fn(f32, f32) -> f32 + Send + Sync>,
-    dy: Box<dyn Fn(f32, f32) -> f32 + Send + Sync>,
-}
+pub trait ParametricCurve: Send + Sync {
+    fn position(&self, t: f32) -> Vec2;
+    fn velocity(&self, t: f32) -> Vec2;
+    fn a(t: f32) -> f32;
 
-impl PositionAndVelocity {
-    pub fn new<X, Y, DX, DY>(x: X, y: Y, dx: DX, dy: DY) -> Self
-    where
-        X: Fn(f32, f32) -> f32 + Send + Sync + 'static,
-        Y: Fn(f32, f32) -> f32 + Send + Sync + 'static,
-        DX: Fn(f32, f32) -> f32 + Send + Sync + 'static,
-        DY: Fn(f32, f32) -> f32 + Send + Sync + 'static,
-    {
-        Self {
-            x: Box::new(x),
-            y: Box::new(y),
-            dx: Box::new(dx),
-            dy: Box::new(dy),
-        }
+    fn s(&self, t: f32) -> f32 {
+        let func = |theta: f32| {
+            let vel = self.velocity(theta);
+            (vel.x.powi(2) + vel.y.powi(2)).powf(0.5)
+        };
+        let upper_limit: f32 = t;
+        static LOWER_LIMIT: f32 = 0.;
+        static N: usize = 50;
+
+        legendre_rule(func, LOWER_LIMIT, upper_limit, N) as f32
     }
 
-    fn s(&self, t: f32, theta: f32) -> f32 {
-        let func = |t: f32| ((self.dx)(t, theta).powi(2) + (self.dy)(t, theta).powi(2)).sqrt();
-        let lower_limit = 0.;
-        let upper_limit = t;
-        let n = 50_usize;
-        legendre_rule(func, lower_limit, upper_limit, n) as f32
-    }
-
-    fn n(&self, t: f32, theta: f32) -> Vec<f32> {
-        let ds = ((self.dx)(t, theta).powi(2) + (self.dy)(t, theta).powi(2)).sqrt();
+    fn n(&self, t: f32) -> Vec2 {
+        let vel = self.velocity(t);
+        let ds = (vel.x.powi(2) + vel.y.powi(2)).powf(0.5);
         if ds < 1e-6 {
-            vec![1., 0.]
+            Vec2::new(1., 0.)
         } else {
-            vec![-(self.dy)(t, theta) / ds, (self.dx)(t, theta) / ds]
+            Vec2::new(-vel.y, vel.x) / ds
         }
     }
+}
 
-    fn normal_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let current_s = self.s(t, theta);
-        a(theta) * (current_s / ta).sin()
+pub trait WaveTransform: ParametricCurve {
+    fn normal_offset(&self, t: f32, ta: f32) -> f32 {
+        let current_s = self.s(t);
+        Self::a(t) * (current_s / ta).sin()
     }
 
-    pub fn x_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let normal = self.n(t, theta);
-        (self.x)(t, theta) + self.normal_offset(t, theta, a, ta) * normal[0]
-    }
-
-    pub fn y_offset(&self, t: f32, theta: f32, a: &dyn Fn(f32) -> f32, ta: f32) -> f32 {
-        let normal = self.n(t, theta);
-        (self.y)(t, theta) + self.normal_offset(t, theta, a, ta) * normal[1]
+    fn offset(&self, t: f32, ta: f32) -> Vec2 {
+        let normal = self.n(t);
+        self.position(t) + normal * self.normal_offset(t, ta)
     }
 }
+
+impl<T: ParametricCurve> WaveTransform for T {}
